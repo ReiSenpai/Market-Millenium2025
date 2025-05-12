31e795f8dd27853a6495864cd5e393a57f4ca373
Joseph-RC <rc.josephmark@gmail.com>
2025-05-12 03:19:40 -0500

    solo mayuscula a la clase

--- src/main/java/com/marketMillenium/marketmillenium/controladores/controlador.java
+++ src/main/java/com/marketMillenium/marketmillenium/controladores/controlador.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controlador {
+public class Controlador {
     @GetMapping({"/", "/index"})
     public String inicio(Model model) {
         return "index";
